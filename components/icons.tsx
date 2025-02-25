c82bee1448a6765b63b6e2612fac3b057ccbc5b3
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2025-02-24 22:08:20 -0300

    Add services lists with icons

--- components/icons.tsx
+++
@@ -1,8 +0,0 @@
-export const Service = () => (<svg width="33" height="32" viewBox="0 0 33 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<path d="M21.8333 21.3333L25.8333 10.6666L29.8333 21.3333C28.6733 22.2 27.2733 22.6666 25.8333 22.6666C24.3933 22.6666 22.9933 22.2 21.8333 21.3333Z" stroke="#1D8084" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-	<path d="M3.16666 21.3333L7.16666 10.6666L11.1667 21.3333C10.0067 22.2 8.60666 22.6666 7.16666 22.6666C5.72666 22.6666 4.32666 22.2 3.16666 21.3333Z" stroke="#1D8084" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-	<path d="M9.83334 28H23.1667" stroke="#1D8084" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-	<path d="M16.5 4V28" stroke="#1D8084" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-	<path d="M4.5 9.33329H7.16667C9.83333 9.33329 13.8333 7.99996 16.5 6.66663C19.1667 7.99996 23.1667 9.33329 25.8333 9.33329H28.5" stroke="#1D8084" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-</svg>
-)
--- components/services.tsx
+++ components/services.tsx
@@ -1,4 +1,4 @@
-import { Service } from "./icons";
+import { BookText, BriefcaseBusiness, Building2, GraduationCapIcon, Handshake, NotebookText, Scale, SquareActivity, Trees } from "lucide-react";
 
 export default function Services() {
 	return (
@@ -10,42 +10,82 @@ export default function Services() {
 
 			<div className="mt-20 border border-slate-200 px-8 py-12 relative">
 				<div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 bg-white"><h3 className="font-semibold text-3xl font-serif text-center">Para Gobiernos Locales y ONGs</h3></div>
-				<ul className="grid grid-cols-3 gap-12">
+				<ul className="grid grid-cols-3 gap-x-12 gap-y-8">
 					<li className="flex flex-col items-center gap-3">
-						<Service />
+						<BriefcaseBusiness className="text-primary-600" size={32} absoluteStrokeWidth />
 						<h4 className="font-serif font-semibold text-lg text-center">Asesoría y Consultoría Jurídica</h4>
-						<p className="text-center text-sm text-slate-800">Reformas Constitucionales y Legislativas. <br /> Diseño e Implementación de Políticas Públicas. <br /> Control de Constitucionalidad y Convencionalidad. <br /> Litigio Estratégico.</p>
+						<p className="text-center text-sm text-slate-700">Reformas Constitucionales y Legislativas. <br /> Diseño e Implementación de Políticas Públicas. <br /> Control de Constitucionalidad y Convencionalidad. <br /> Litigio Estratégico.</p>
 					</li>
 
 					<li className="flex flex-col items-center gap-3">
-						<Service />
-						<h4 className="font-serif font-semibold text-lg text-center">Asesoría y Consultoría Jurídica</h4>
-						<p className="text-center text-sm text-slate-800">Reformas Constitucionales y Legislativas. <br /> Diseño e Implementación de Políticas Públicas. <br /> Control de Constitucionalidad y Convencionalidad. <br /> Litigio Estratégico.</p>
+						<GraduationCapIcon size={32} className="text-primary-600" absoluteStrokeWidth />
+						<h4 className="font-serif font-semibold text-lg text-center">Capacitación y Formación</h4>
+						<p className="text-center text-sm text-slate-700">Formación en Teoría Constitucional y Derechos Humanos.</p>
 					</li>
 
 					<li className="flex flex-col items-center gap-3">
-						<Service />
-						<h4 className="font-serif font-semibold text-lg text-center">Asesoría y Consultoría Jurídica</h4>
-						<p className="text-center text-sm text-slate-800">Reformas Constitucionales y Legislativas. <br /> Diseño e Implementación de Políticas Públicas. <br /> Control de Constitucionalidad y Convencionalidad. <br /> Litigio Estratégico.</p>
+						<NotebookText size={32} className="text-primary-600" absoluteStrokeWidth />
+						<h4 className="font-serif font-semibold text-lg text-center">Investigación y Publicaciones Especializadas</h4>
+						<p className="text-center text-sm text-slate-700">Investigación Académica y Jurídica. <br /> Publicación especializada. <br /> Elaboración de Recomendaciones.</p>
 					</li>
 
 					<li className="flex flex-col items-center gap-3">
-						<Service />
-						<h4 className="font-serif font-semibold text-lg text-center">Asesoría y Consultoría Jurídica</h4>
-						<p className="text-center text-sm text-slate-800">Reformas Constitucionales y Legislativas. <br /> Diseño e Implementación de Políticas Públicas. <br /> Control de Constitucionalidad y Convencionalidad. <br /> Litigio Estratégico.</p>
+						<Building2 size={32} className="text-primary-600" absoluteStrokeWidth />
+						<h4 className="font-serif font-semibold text-lg text-center">Incidencia Política y Participación Ciudadana</h4>
+						<p className="text-center text-sm text-slate-700">Estrategias de Incidencia en Reformas normativas. <br /> Fortalecimiento de la Sociedad Civil.</p>
 					</li>
 
 					<li className="flex flex-col items-center gap-3">
-						<Service />
-						<h4 className="font-serif font-semibold text-lg text-center">Asesoría y Consultoría Jurídica</h4>
-						<p className="text-center text-sm text-slate-800">Reformas Constitucionales y Legislativas. <br /> Diseño e Implementación de Políticas Públicas. <br /> Control de Constitucionalidad y Convencionalidad. <br /> Litigio Estratégico.</p>
+						<SquareActivity size={32} className="text-primary-600" absoluteStrokeWidth />
+						<h4 className="font-serif font-semibold text-lg text-center">Monitoreo y Seguimiento de Reformas Normativas</h4>
+						<p className="text-center text-sm text-slate-700">Monitoreo de Reformas Constitucionales y Normativas. <br /> Evaluación de Políticas Públicas.</p>
 					</li>
 
 					<li className="flex flex-col items-center gap-3">
-						<Service />
-						<h4 className="font-serif font-semibold text-lg text-center">Asesoría y Consultoría Jurídica</h4>
-						<p className="text-center text-sm text-slate-800">Reformas Constitucionales y Legislativas. <br /> Diseño e Implementación de Políticas Públicas. <br /> Control de Constitucionalidad y Convencionalidad. <br /> Litigio Estratégico.</p>
+						<BookText size={32} className="text-primary-600" absoluteStrokeWidth />
+						<h4 className="font-serif font-semibold text-lg text-center">Elaboración de Dictámenes, Ordenanzas, Leyes y Fundamentaciones</h4>
+						<p className="text-center text-sm text-slate-700">Dictámenes Jurídicos Especializados. <br /> Redacción de Leyes, Ordenanzas y Reglamentos Locales.</p>
 					</li>
+				</ul>
+			</div>
+
+			<div className="mt-20 border border-slate-200 px-8 py-12 relative">
+				<div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 p-4 bg-white"><h3 className="font-semibold text-3xl font-serif text-center">Para Empresas y el Sector Privado</h3></div>
+				<ul className="grid grid-cols-3 gap-x-12 gap-y-8">
+					<li className="flex flex-col items-center gap-3">
+						<Scale size={32} className="text-primary-600" absoluteStrokeWidth />
+						<h4 className="font-serif font-semibold text-lg text-center">Cumplimiento Normativo y Gobernanza Corporativa</h4>
+						<p className="text-center text-sm text-slate-700">Alineación Regulatoria y Cumplimiento Constitucional. <br /> Auditorías y Evaluaciones de Cumplimiento. <br /> Diseño e Implementación de Políticas Internas. <br /> Análisis de Riesgos.</p>
+					</li>
+
+					<li className="flex flex-col items-center gap-3">
+						<GraduationCapIcon size={32} className="text-primary-600" absoluteStrokeWidth />
+						<h4 className="font-serif font-semibold text-lg text-center">Capacitación y Desarrollo en Gobernanza Empresarial</h4>
+						<p className="text-center text-sm text-slate-700">Formación en Derechos Humanos y Empresas.</p>
+					</li>
+
+					<li className="flex flex-col items-center gap-3">
+						<Trees className="text-primary-600" size={32} absoluteStrokeWidth />
+						<h4 className="font-serif font-semibold text-lg text-center">Impacto Social y Ambiental</h4>
+						<p className="text-center text-sm text-slate-700">Evaluación de Impacto en Derechos Humanos y Medio Ambiente. <br /> Diseño de Estrategias de Mitigación.</p>
+					</li>
+
+					<div className="col-span-3 mx-32">
+						<div className="grid grid-cols-2 gap-12">
+							<li className="flex flex-col items-center gap-3 max-w-sm">
+								<Handshake size={32} className="text-primary-600" absoluteStrokeWidth />
+								<h4 className="font-serif font-semibold text-lg text-center">Estrategias de Incidencia y Responsabilidad Social Empresarial</h4>
+								<p className="text-center text-sm text-slate-700">Participación en Políticas Públicas y Alianzas Estratégicas.</p>
+							</li>
+
+							<li className="flex flex-col items-center gap-3 max-w-sm">
+								<SquareActivity size={32} className="text-primary-600" absoluteStrokeWidth />
+								<h4 className="font-serif font-semibold text-lg text-center">Monitoreo y Evaluación para Empresas</h4>
+								<p className="text-center text-sm text-slate-700">Seguimiento de Cumplimiento Normativo. <br /> Análisis del Entorno Legal y Regulatorio.</p>
+							</li>
+						</div>
+					</div>
+
 				</ul>
 			</div>
 		</section>
