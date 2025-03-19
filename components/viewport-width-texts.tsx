450a7055eca2095481a0c14f119fce0a6fc53e2d
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2025-03-19 17:55:02 -0300

    Fix text sizes of vw texts outside instagram

--- components/viewport-width-texts.tsx
+++ components/viewport-width-texts.tsx
@@ -6,7 +6,7 @@ export function HeroTitle() {
 	const { scalingValue } = useScalingValue()
 
 	return (
-		<h1 style={scalingValue < 1 && window.innerWidth < 768 ? { fontSize: `${8 * scalingValue}vw` } : { fontSize: '8vw' }} className="font-serif md:text-5xl leading-tight lg:text-6xl 2xl:text-7xl font-semibold">Construyendo el marco <br /> ideal para tu institución</h1>
+		<h1 style={scalingValue < 1 && window.innerWidth < 768 ? { fontSize: `${8 * scalingValue}vw` } : {}} className="font-serif text-[8vw] md:text-5xl leading-tight lg:text-6xl 2xl:text-7xl font-semibold">Construyendo el marco <br /> ideal para tu institución</h1>
 	)
 }
 
@@ -14,7 +14,7 @@ export function ServicesSubTitle({ content }: { content: string }) {
 	const { scalingValue } = useScalingValue()
 
 	return (
-		<h3 style={scalingValue < 1 && window.innerWidth < 768 ? { fontSize: `${5.5 * scalingValue}vw` } : { fontSize: '5.5vw' }} className="font-semibold sm:text-3xl whitespace-nowrap font-serif text-center">
+		<h3 style={scalingValue < 1 && window.innerWidth < 768 ? { fontSize: `${5.5 * scalingValue}vw` } : {}} className="font-semibold text-[5.5vw] sm:text-3xl whitespace-nowrap font-serif text-center">
 			{content}
 		</h3>
 	)
